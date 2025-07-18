02ec765136be8e6edfb68558fe20b94b5e93f044
engrjunaidali <engrjunaidali0@gmail.com>
2025-07-19 00:44:13 +0500

    used group routes

--- start/routes.ts
+++ start/routes.ts
@@ -14,16 +14,21 @@ import router from '@adonisjs/core/services/router'
 router.get('/', ({ inertia }) => inertia.render('home'))
 // router.get('/todos', ({ inertia }) => inertia.render('todos/empty'))
 
-router.get('/todos', [TodosController, 'index'])  // Fetch all todos
-router.post('/todos', [TodosController, 'store'])  // Create a new todo
-router.get('/todos/:id', [TodosController, 'show'])  // Get a single todo
-router.put('/todos/:id', [TodosController, 'update'])  // Update a todo
-router.delete('/todos/:id', [TodosController, 'destroy'])  // Delete a todo
+
+router.group(() => {
+    router.get('/', [TodosController, 'index'])        // GET /todos
+    router.post('/', [TodosController, 'store'])       // POST /todos
+    router.get('/:id', [TodosController, 'show'])      // GET /todos/:id
+    router.put('/:id', [TodosController, 'update'])    // PUT /todos/:id
+    router.delete('/:id', [TodosController, 'destroy']) // DELETE /todos/:id
+    router.post('/upload', [TodosController, 'uploadImage'])
+}).prefix('/todos')
 
 // image upload route
-router.post('/todos/upload', [TodosController, 'uploadImage'])
-    
-router.get('/notes', [NotesController, 'index'])
-router.post('/notes', [NotesController, 'store'])
-router.put('/notes/:id', [NotesController, 'update'])
-router.delete('/notes/:id', [NotesController, 'destroy'])
+
+router.group(() => {
+  router.get('/', [NotesController, 'index'])        // GET /notes
+  router.post('/', [NotesController, 'store'])       // POST /notes
+  router.put('/:id', [NotesController, 'update'])    // PUT /notes/:id
+  router.delete('/:id', [NotesController, 'destroy']) // DELETE /notes/:id
+}).prefix('/notes')
