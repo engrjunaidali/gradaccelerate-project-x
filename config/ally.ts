ccb71b8c82c614ef338bd502a4b2362abefd7d62
engrjunaidali <engrjunaidali0@gmail.com>
2025-07-23 00:08:34 +0500

    D8 T4: google auth for notes and todos - Commit 3

--- config/ally.ts
+++ config/ally.ts
@@ -3,8 +3,8 @@ import { defineConfig, services } from '@adonisjs/ally'
 
 const allyConfig = defineConfig({
   google: services.google({
-    clientId: env.get('GOOGLE_CLIENT_ID') || '404309581900-jrusna7s72crd8nrffsoonk4k0qfitu4.apps.googleusercontent.com',
-    clientSecret: env.get('GOOGLE_CLIENT_SECRET') || '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',
+    clientId: env.get('GOOGLE_CLIENT_ID'),
+    clientSecret: env.get('GOOGLE_CLIENT_SECRET'),
     callbackUrl: 'http://localhost:3333/auth/google/callback',
   }),
 })
