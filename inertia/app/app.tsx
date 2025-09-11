1938bcff42ece27dd7b96054c36d91c6c6d07b11
engrjunaidali <engrjunaidali0@gmail.com>
2025-09-11 15:50:33 +0500

    D15 T1: integrated backend sentry for real time error tracking  - Commit 1

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -5,8 +5,20 @@ import '../css/app.css';
 import { hydrateRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
-
+import * as Sentry from "@sentry/react";
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
+
+Sentry.init({ 
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: true
+ });
+ // Capture an exception
+try {
+  console.log('About to throw an error');
+  throw new Error("Test error 2");
+} catch (e) {
+  Sentry.captureException(e);
+}
 
 createInertiaApp({
   progress: { color: '#5468FF' },
